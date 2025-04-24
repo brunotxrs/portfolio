404a14882cd0070e670254940ed207eb25f5334f
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-24 17:56:28 -0400

    feat: import icons socias, create new elements, and stylings this new elements

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,20 @@
 import React from "react";
-
 import './header.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faGithubAlt, faLinkedin, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 
-function Header() {
 
+function Header({onAboutMeClick, onProjectClick}) {
+  
     return (
         <div>
             <div className="container-header">
 
                 <div className="navigation">
                     <ul>
-                        <li>Sobre Mim</li>
-                        <li>Projetos</li>
+                        <li onClick={onAboutMeClick}>Sobre Mim</li>
+                        <li onClick={onProjectClick}>Projetos</li>
                         <li>Skills</li>
                         <li>Contato</li>
                     </ul>
@@ -27,6 +29,23 @@ function Header() {
                         <img src="https://raw.githubusercontent.com/brunotxrs/dev-links/d0021d8d8e5a663121f72bf62353a067a091eb8a/src/assets/img/avatar-light.svg" alt="imagem de Bruno Teixeira de Óculos" />
                     </div>
                 </div>
+
+                <div className="about">
+                    <p>Desenvolvedor Front-End com paixão por transformar ideias em experiências web impactantes.</p>
+                </div>
+
+                <div className="link-social">
+                    <ul>
+                        <li><a href="https://github.com/brunotxrs" target="_blank">
+                            <FontAwesomeIcon className="social" icon={faGithub} />
+                        </a></li>
+                        <li><a href="https://www.linkedin.com/in/brunotxrs/" target="_blank"><FontAwesomeIcon className="linkedin" icon={faLinkedinIn} /></a></li>
+                        
+                        <li></li>
+                    </ul>
+                </div>
+
+                
 
             </div>
 
