410edcfa8f67fba2943061a87f8767e2f961e8d7
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-24 17:57:17 -0400

    feat: create the component AboutMe, and create your styles

---
+++ src/components/about-me/about-me.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./about-me.css";
+
+
+
+function AboutMe() {
+
+
+    return(
+        <div>
+
+            <div className="container-about-me">
+                <h2>Sobre Mim</h2>
+                <p>Desenvolvedor Front-End com paixão por transformar ideias em experiências web impactantes. Experiente na implementação de layouts e componentes a partir de designs Figma, possuo sólidos conhecimentos em HTML5, CSS3, JavaScript (ES6+) e React.js. Busco colaborar em equipes multidisciplinares para criar interfaces acessíveis e de alcance global.</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default AboutMe;
