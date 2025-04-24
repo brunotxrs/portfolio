37b72b36ecbbb875064600c01be68bb53096d145
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-24 17:58:53 -0400

    feat: create one anchorage for element aboutMe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useRef } from 'react';
 import './App.css';
+import AboutMe from './components/about-me/about-me';
 import Header from './components/header/header';
 
 function App() {
+  const aboutMeRef = useRef(null);
+
+
+
+  const scrollToAboutMe = () => {
+    aboutMeRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div>
-      <Header />
-      
+      <Header onAboutMeClick={scrollToAboutMe}/>
+      <div ref={aboutMeRef}>
+        <AboutMe />
+      </div>
+
+    
     </div>
   );
 }
