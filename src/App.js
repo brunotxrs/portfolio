bdcb2a160bde4fa0e773c35e2cbf01f68d32c489
Bruno Teixeira <183097017+brunotxrs@users.noreply.github.com>
2025-04-26 11:45:16 -0400

    feat: import the component project

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import { useRef } from 'react';
 import './App.css';
 import Header from './components/header/header';
 import Skills from './components/skills/skills';
+import Project from './components/project/project';
 
 function App() {
   const skillsRef = useRef(null);
+  const projectRef = useRef(null);
 
 
   const scrollToSkills = () => {
     skillsRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
+  const scrollToProject = () => {
+    projectRef.current?.scrollIntoView({behavior:'smooth'})
+  }
 
   return (
     <div>
-      <Header onSkillsClick={scrollToSkills}/>
+      <Header onSkillsClick={scrollToSkills} onProjectClick={scrollToProject}/>
       <div ref={skillsRef}>
         <Skills />
+      </div>
+      <div ref={projectRef}>
+        <Project />
       </div>
 
     
